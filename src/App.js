1c1e701ffe9163d9b8a1b04deb1d7d071a97e690
nunsaramengel <nunsaramengel@users.noreply.github.com>
2021-11-01 20:29:14 +0100

    Task Manager Tutorial

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
-
+import Header from './Header'
 function App() {
+
+  const name = "Fang"
   return (
+    
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <Header/>{name}
+     <ul class="pfeffer">
+      <li>hey</li>
+      <li>Schnitzel</li>
+    </ul>
     </div>
   );
 }
